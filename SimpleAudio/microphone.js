737b64c22793f11d0a3ec1df45020b512f28bb09
Rendall <rendall@gmail.com>
2019-01-18 12:29:27 +0200

    put microphone into Microphone execution context

--- SimpleAudio/microphone.js
+++ SimpleAudio/microphone.js
@@ -1,31 +1,34 @@
 "use strict";
-let audioRecorder = null;
-const onAudioDataAvailable = (e) => {
-    const blob = new Blob([e.data], { 'type': 'audio/ogg; codecs=opus' });
-    const audioURL = URL.createObjectURL(blob);
-    const audio = document.getElementById('audio');
-    audio.controls = true;
-    audio.src = audioURL;
-    audio.play();
+const Microphone = () => {
+    let audioRecorder = null;
+    const onAudioDataAvailable = (e) => {
+        const mimeType = e.target.mimeType; // default mimetype changes depending on browser
+        const blob = new Blob([e.data], { type: mimeType });
+        const audioURL = URL.createObjectURL(blob);
+        const audio = document.getElementById('audio');
+        audio.controls = true;
+        audio.src = audioURL;
+        audio.play();
+    };
+    const onAudioStart = (e) => document.querySelector('button#record_btn').innerHTML = "STOP";
+    const onAudioStop = (e) => document.querySelector('button#record_btn').innerHTML = "RECORD";
+    const onRecordClick = (e) => {
+        if (audioRecorder && audioRecorder.state === "recording") {
+            audioRecorder.stop();
+        }
+        else
+            navigator.mediaDevices.getUserMedia({ audio: true, video: false }) // getUserMedia works only in 'localhost' or 'https://'
+                .then((stream) => {
+                if (!audioRecorder) {
+                    audioRecorder = new MediaRecorder(stream);
+                    audioRecorder.addEventListener("start", onAudioStart);
+                    audioRecorder.addEventListener("stop", onAudioStop);
+                    audioRecorder.addEventListener("dataavailable", onAudioDataAvailable);
+                }
+                audioRecorder.start();
+            })
+                .catch(err => console.log('err', JSON.stringify(err)));
+    };
+    document.querySelector('button#record_btn').addEventListener('click', onRecordClick);
 };
-const onAudioStart = (e) => document.querySelector('button#record_btn').innerHTML = "STOP";
-const onAudioStop = (e) => document.querySelector('button#record_btn').innerHTML = "RECORD";
-const onRecordClick = (e) => {
-    console.log(e, audioRecorder);
-    if (audioRecorder && audioRecorder.state === "recording") {
-        audioRecorder.stop();
-    }
-    else
-        navigator.mediaDevices.getUserMedia({ audio: true, video: false }) // getUserMedia works only in 'localhost' or 'https://'
-            .then((stream) => {
-            if (!audioRecorder) {
-                audioRecorder = new MediaRecorder(stream);
-                audioRecorder.addEventListener("start", onAudioStart);
-                audioRecorder.addEventListener("stop", onAudioStop);
-                audioRecorder.addEventListener("dataavailable", onAudioDataAvailable);
-            }
-            audioRecorder.start();
-        })
-            .catch(err => console.log('err', JSON.stringify(err)));
-};
-document.querySelector('button#record_btn').addEventListener('click', onRecordClick);
+Microphone();
